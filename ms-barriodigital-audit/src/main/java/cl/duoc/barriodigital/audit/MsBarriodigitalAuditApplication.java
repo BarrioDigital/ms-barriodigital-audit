9b047c1122696622acef38de9b7d9b1cae7ce9fb
diat07 <arayadiego6467@gmail.com>
2026-09-11 16:54:03 -0300

    chore: inicializar microservicio de auditoría

---
+++ ms-barriodigital-audit/src/main/java/cl/duoc/barriodigital/audit/MsBarriodigitalAuditApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.barriodigital.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBarriodigitalAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBarriodigitalAuditApplication.class, args);
+	}
+
+}
